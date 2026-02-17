3556474dc005f556e37d9dd15b712a7e99cbf121
Nmx11 <55904108+Nmx11@users.noreply.github.com>
2026-02-16 23:48:29 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app_silvia.py
+++ app_silvia.py
@@ -410,9 +410,9 @@ with tab1:
                 
         st.subheader("3. Diseño de Placa")
         texto_en_foto = st.text_input("Texto SOBRE la imagen:", value=st.session_state.get('frase_para_placa', ""))
-        if st.button("👁️ Actualizar texto en foto"):
-        st.session_state.frase_para_placa = texto_en_foto
-        st.rerun()
+            if st.button("👁️ Actualizar texto en foto"):
+            st.session_state.frase_para_placa = texto_en_foto
+            st.rerun()
         
         c_p1, c_p2, c_p3 = st.columns(3) 
         with c_p1:
@@ -488,6 +488,7 @@ with col_preview:
     </div>
     """
     st.markdown(html_post, unsafe_allow_html=True)
+
 
 
 
